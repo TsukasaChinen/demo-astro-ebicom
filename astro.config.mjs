700e4c0ca83d4026c97005f248d5bc6168cda8ff
Tsukasa Chinen <100554290+TsukasaChinen@users.noreply.github.com>
2023-04-09 23:38:24 +0900

    デブロイ前のプッシュ

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config";
+import { siteMeta } from "./src/lib/constants";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+const {
+  siteUrl
+} = siteMeta;
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: siteUrl,
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "nord",
+      langs: [],
+      wrap: true
+    }
+  }
+});
