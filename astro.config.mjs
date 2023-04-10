5f9493c03a8e58fdd2330c54c1ae0c36bdde2a94
Tsukasa Chinen <100554290+TsukasaChinen@users.noreply.github.com>
2023-04-10 11:06:36 +0900

    Add Prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,24 @@ import { defineConfig } from "astro/config";
 import { siteMeta } from "./src/lib/constants";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-const {
-  siteUrl
-} = siteMeta;
-
+import prefetch from "@astrojs/prefetch";
+const { siteUrl } = siteMeta;
 
 // https://astro.build/config
 export default defineConfig({
   site: siteUrl,
-  integrations: [image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx()],
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    prefetch(),
+  ],
   markdown: {
     shikiConfig: {
       theme: "nord",
       langs: [],
-      wrap: true
-    }
-  }
+      wrap: true,
+    },
+  },
 });
