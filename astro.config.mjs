bd30b8faeb474b2c5f06823b4eab0ad21bcb5bbb
Tsukasa Chinen <100554290+TsukasaChinen@users.noreply.github.com>
2023-04-10 11:12:16 +0900

    Add Sitemap and RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,23 @@ import { siteMeta } from "./src/lib/constants";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
-const { siteUrl } = siteMeta;
+import sitemap from "@astrojs/sitemap";
+const {
+  siteUrl
+} = siteMeta;
+
 
 // https://astro.build/config
 export default defineConfig({
   site: siteUrl,
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    prefetch(),
-  ],
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), prefetch(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "nord",
       langs: [],
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
